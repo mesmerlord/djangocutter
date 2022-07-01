b569e3192cceae1dbb9346c27486d3bad167fbbc
Govind <76161333+mesmerlord@users.noreply.github.com>
2022-07-01 18:31:05 +0200

    added person field on excercise

--- django_cutter/fitness/models.py
+++ django_cutter/fitness/models.py
@@ -21,6 +21,7 @@ class Workout(models.Model):
 class Exercise(models.Model):
     name = models.CharField(max_length=100)
     sets = models.ManyToManyField('Set')
+    person = models.ForeignKey('users.User', related_name='exercises', on_delete=models.CASCADE, null = True, blank = True)
 
     def __str__(self):
         return self.name
