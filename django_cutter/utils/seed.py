58e580ae8a3c8b49506b48b804cb626b7c60e219
Govind <76161333+mesmerlord@users.noreply.github.com>
2022-12-17 01:55:39 +0200

    fix: fixed order

--- django_cutter/utils/seed.py
+++ django_cutter/utils/seed.py
@@ -2,7 +2,7 @@ from .factories import SetFactory, WorkoutFactory, ExerciseFactory
 
 def seed():
     for _ in range(100):
-        set_instance = SetFactory.create_batch(size=100)
         workout_instance = WorkoutFactory.create_batch(size=100)
+        set_instance = SetFactory.create_batch(size=100)
         exercise_instance = ExerciseFactory.create_batch(size=100)
     
