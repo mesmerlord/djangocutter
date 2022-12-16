68bce110b14d8e1e57054a7c8f8432a4f1a18863
Govind <76161333+mesmerlord@users.noreply.github.com>
2022-12-17 01:58:15 +0200

    fix: fixed factory

--- django_cutter/utils/factories.py
+++ django_cutter/utils/factories.py
@@ -3,7 +3,7 @@ from django_cutter.fitness.models import Set, Workout, Exercise
 from django_cutter.users.tests.factories import UserFactory
 class SetFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = 'Set'
+        model = Set
 
     name = factory.Faker('word')
     reps = factory.Faker('random_int')
@@ -13,14 +13,14 @@ class SetFactory(factory.django.DjangoModelFactory):
 
 class WorkoutFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = 'Workout'
+        model = Workout
 
     name = factory.Faker('word')
     date = factory.Faker('date_this_decade')
 
 class ExerciseFactory(factory.django.DjangoModelFactory):
     class Meta:
-        model = 'Exercise'
+        model = Exercise
 
     name = factory.Faker('word')
     person = factory.SubFactory(UserFactory)
