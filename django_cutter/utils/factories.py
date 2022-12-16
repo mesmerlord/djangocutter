0b703927ece41cbe02452f5646bfdfd930b9b53e
Govind <76161333+mesmerlord@users.noreply.github.com>
2022-12-17 01:42:56 +0200

    fix: fixed factory

--- django_cutter/utils/factories.py
+++ django_cutter/utils/factories.py
@@ -8,7 +8,7 @@ class SetFactory(factory.django.DjangoModelFactory):
     reps = factory.Faker('random_int')
     weight = factory.Faker('random_int')
     date = factory.Faker('date_this_decade')
-    workout = factory.SubFactory('utils.WorkoutFactory')
+    workout = factory.SubFactory('utils.seed.WorkoutFactory')
 
 class WorkoutFactory(factory.django.DjangoModelFactory):
     class Meta:
