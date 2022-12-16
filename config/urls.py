84524ace1d6a9c278fcaca13d9b065b3270ebb51
Govind <76161333+mesmerlord@users.noreply.github.com>
2022-12-17 01:20:47 +0200

    fix: removed debug toolbar

--- config/urls.py
+++ config/urls.py
@@ -55,7 +55,4 @@ if settings.DEBUG:
         ),
         path("500/", default_views.server_error),
     ]
-    if "debug_toolbar" in settings.INSTALLED_APPS:
-        import debug_toolbar
-
-        urlpatterns = [path("__debug__/", include(debug_toolbar.urls))] + urlpatterns
+    
