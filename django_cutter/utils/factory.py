576bbb6a81bd55e0b9aa0380dc34d40ce27efe9c
Govind <76161333+mesmerlord@users.noreply.github.com>
2022-12-17 01:04:44 +0200

    feat: added factory

---
+++ django_cutter/utils/factory.py
@@ -0,0 +1,26 @@
+import factory
+
+class SetFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'Set'
+
+    name = factory.Faker('word')
+    reps = factory.Faker('random_int')
+    weight = factory.Faker('random_int')
+    date = factory.Faker('date_this_decade')
+    workout = factory.SubFactory('WorkoutFactory')
+
+class WorkoutFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'Workout'
+
+    name = factory.Faker('word')
+    date = factory.Faker('date_this_decade')
+
+class ExerciseFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = 'Exercise'
+
+    name = factory.Faker('word')
+    sets = factory.SubFactory(SetFactory, workout=None)
+    person = factory.SubFactory('users.UserFactory')
