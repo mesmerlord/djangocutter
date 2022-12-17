56b94b02e582ceee0e6db910ed7885992b773701
Govind Siwakoti <mesmer@mail.com>
2022-12-17 18:44:51 +0200

    fix: fixed serializer

--- django_cutter/fitness/serializers.py
+++ django_cutter/fitness/serializers.py
@@ -3,21 +3,22 @@ from rest_framework import serializers
 
 from .models import Set, Exercise, Workout
 
+class WorkoutSerializer(serializers.ModelSerializer):
+    
+    class Meta:
+        model = Workout
+        fields = "__all__"
 
 class SetSerializer(serializers.ModelSerializer):
+    workout = WorkoutSerializer()
 
     class Meta:
         model = Set
         fields = "__all__"
 
 class ExerciseSerializer(serializers.ModelSerializer):
-
+    sets = SetSerializer(many=True)
     class Meta:
         model = Exercise
         fields = "__all__"
 
-class WorkoutSerializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Workout
-        fields = "__all__"
